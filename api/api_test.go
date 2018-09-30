55d15e36b3b58dec6048c6b55e80fd1bbbab83da
Yun Xu <yunx@zillowgroup.com>
2018-09-30 13:08:33 -0700

    add unit tests for api

--- api/api_test.go
+++ api/api_test.go
@@ -2,28 +2,53 @@ package api
 
 import (
     "testing"
+    "encoding/json"
     "net/http"
-	"net/http/httptest"
-	"go.uber.org/zap"
-	"github.com/stretchr/testify/assert"
-	"github.com/yunstanford/carbon-table/cfg"
-	"github.com/yunstanford/carbon-table/table"
+    "net/http/httptest"
+    "go.uber.org/zap"
+    "github.com/stretchr/testify/assert"
+    "github.com/yunstanford/carbon-table/cfg"
+    "github.com/yunstanford/carbon-table/table"
+    "github.com/yunstanford/carbon-table/trie"
 )
 
 
 // Test Api Full
 func TestApiFull(t *testing.T) {
-	// Setup
-	config := cfg.NewConfig()
-	tbl := table.NewTable(config.Table)
-	logger, _ := zap.NewProduction()
-	a := NewApi(config.Api, tbl, logger)
+    // Setup
+    config := cfg.NewConfig()
+    tbl := table.NewTable(config.Table)
+    logger, _ := zap.NewProduction()
+    a := NewApi(config.Api, tbl, logger)
 
-	// Test
-	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/ping", nil)
-	a.router.ServeHTTP(w, req)
+    tbl.Insert("carbon.cache.a")
 
-	assert.Equal(t, 200, w.Code)
-	assert.Equal(t, "{\"message\":\"pong\"}", w.Body.String())
+    // Test Ping
+    w := httptest.NewRecorder()
+    req, _ := http.NewRequest("GET", "/ping", nil)
+    a.router.ServeHTTP(w, req)
+
+    assert.Equal(t, 200, w.Code)
+    assert.Equal(t, "{\"message\":\"pong\"}", w.Body.String())
+
+    // Test ExpandQuery
+    w = httptest.NewRecorder()
+    req, _ = http.NewRequest("GET", "/metric/query/carbon.cache.*/", nil)
+    a.router.ServeHTTP(w, req)
+
+    assert.Equal(t, 200, w.Code)
+    assert.Equal(t, "[\"carbon.cache.a\"]", w.Body.String())
+
+    // Test ExpandPattern
+    w = httptest.NewRecorder()
+    req, _ = http.NewRequest("GET", "/metric/pattern/carbon.cache.*/", nil)
+    a.router.ServeHTTP(w, req)
+
+    var queries []trie.QueryResult
+    if err := json.Unmarshal(w.Body.Bytes(), &queries); err != nil {
+        panic(err)
+    }
+
+    assert.Equal(t, 200, w.Code)
+    assert.Equal(t, []trie.QueryResult{trie.QueryResult{Query:"carbon.cache.a", IsLeaf:true}}, queries)
 }
